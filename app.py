ca018741baf044ba3c16a450c615ccd718bcd9a0
RahulArya15 <91099259+RahulArya15@users.noreply.github.com>
2022-02-15 11:09:10 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,89 @@
+
+
+from flask import Flask, render_template, Response
+import cv2
+import numpy as np
+import face_recognition
+import os
+from datetime import datetime
+
+app=Flask(__name__)
+path = 'images'
+images = []
+personNames = []
+myList = os.listdir(path)
+print(myList)
+for cu_img in myList:
+    current_Img = cv2.imread(f'{path}/{cu_img}')
+    images.append(current_Img)
+    personNames.append(os.path.splitext(cu_img)[0])
+print(personNames)
+
+
+def faceEncodings(images):
+    encodeList = []
+    for img in images:
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        encode = face_recognition.face_encodings(img)[0]
+        encodeList.append(encode)
+    return encodeList
+
+
+def attendance(name):
+    with open('Attendance.csv', 'r+') as f:
+        myDataList = f.readlines()
+        nameList = []
+        for line in myDataList:
+            entry = line.split(',')
+            nameList.append(entry[0])
+        if name not in nameList:
+            time_now = datetime.now()
+            tStr = time_now.strftime('%H:%M:%S')
+            dStr = time_now.strftime('%d/%m/%Y')
+            f.writelines(f'\n{name},{tStr},{dStr}')
+
+
+encodeListKnown = faceEncodings(images)
+print('All Encodings Complete!!!')
+
+cap = cv2.VideoCapture(0)
+
+def gen_frames():
+ while True:
+    ret, frame = cap.read()
+    faces = cv2.resize(frame, (0, 0), None, 0.25, 0.25)
+    faces = cv2.cvtColor(faces, cv2.COLOR_BGR2RGB)
+
+    facesCurrentFrame = face_recognition.face_locations(faces)
+    encodesCurrentFrame = face_recognition.face_encodings(faces, facesCurrentFrame)
+
+    for encodeFace, faceLoc in zip(encodesCurrentFrame, facesCurrentFrame):
+        matches = face_recognition.compare_faces(encodeListKnown, encodeFace)
+        faceDis = face_recognition.face_distance(encodeListKnown, encodeFace)
+        # print(faceDis)
+        matchIndex = np.argmin(faceDis)
+
+        if matches[matchIndex]:
+            name = personNames[matchIndex].upper()
+            # print(name)
+            y1, x2, y2, x1 = faceLoc
+            y1, x2, y2, x1 = y1 * 4, x2 * 4, y2 * 4, x1 * 4
+            cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
+            cv2.rectangle(frame, (x1, y2 - 35), (x2, y2), (0, 255, 0), cv2.FILLED)
+            cv2.putText(frame, name, (x1 + 6, y2 - 6), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 2)
+            attendance(name)
+
+    ret, buffer = cv2.imencode('.jpg', frame)
+    frame = buffer.tobytes()
+    yield (b'--frame\r\n'b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+@app.route('/video_feed')
+def video_feed():
+    return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+if __name__=='__main__':
+    app.run(debug=True)
